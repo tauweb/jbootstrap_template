a63deea8fe7852deaee68d766c728bff3c8d2c7a
whiskeyman <whiskeyman.tau@gmail.com>
2016-02-24 12:37:36 +0300

    develop commit

--- tmpls/top-columns.php
+++ tmpls/top-columns.php
@@ -81,9 +81,11 @@
 
 <?php // Top Pre second. Модуль во всю ширину.=================================================== ?>
 <?php if ( $this->countModules('top-pre-second')) :?>
-<div class="row">
-	<div class="col-md-12">
-		<jdoc:include type="modules" name="top-pre-second" style="none" />
+<div class="row top-pre-second">
+	<div class="<?=$classContainer;?>">
+		<div class="col-md-12">
+			<jdoc:include type="modules" name="top-pre-second" style="none" />
+		</div>
 	</div>
 </div>
 <?php echo $module_separator; ?>
@@ -92,61 +94,63 @@
 
 <?php if ( $this->countModules('top-second-left') or $this->countModules('top-second-center') or 
 	$this->countModules('top-second-right') ) : ?>
-<div class="row">
-	<?php // Top 3 small or 2 middle ============================================================== ?>
-	<?php // Left column ?>
-	<?php if ( $this->countModules('top-second-left') and 
-		$this->countModules('top-second-center') and 
-		$this->countModules('top-second-right') ) : ?>
-	<div class="col-md-4">
-		<jdoc:include type="modules" name="top-second-left" style="none" />
-	</div>
-	<?php elseif ( $this->countModules('top-second-left') and 
-		($this->countModules('top-second-center') or $this->countModules('top-second-right')) ) : ?>
-	<div class="col-md-6">
-		<jdoc:include type="modules" name="top-second-left" style="none" />
-	</div>
-	<?php elseif ( $this->countModules('top-second-left') ) : ?>
-	<div class="col-md-12">
-		<jdoc:include type="modules" name="top-second-left" style="none" />
-	</div>
-	<?php endif; ?>
+<div class="row top-second">
+	<div class="<?=$classContainer;?>">
+		<?php // Top 3 small or 2 middle ============================================================== ?>
+		<?php // Left column ?>
+		<?php if ( $this->countModules('top-second-left') and 
+			$this->countModules('top-second-center') and 
+			$this->countModules('top-second-right') ) : ?>
+		<div class="col-md-4">
+			<jdoc:include type="modules" name="top-second-left" style="none" />
+		</div>
+		<?php elseif ( $this->countModules('top-second-left') and 
+			($this->countModules('top-second-center') or $this->countModules('top-second-right')) ) : ?>
+		<div class="col-md-6">
+			<jdoc:include type="modules" name="top-second-left" style="none" />
+		</div>
+		<?php elseif ( $this->countModules('top-second-left') ) : ?>
+		<div class="col-md-12">
+			<jdoc:include type="modules" name="top-second-left" style="none" />
+		</div>
+		<?php endif; ?>
 
-	<?php // Top center column ?>
-	<?php if ( $this->countModules('top-second-center') and 
-		$this->countModules('top-second-left') and 
-		$this->countModules('top-second-right') ) : ?>
-	<div class="col-md-4">
-		<jdoc:include type="modules" name="top-second-center" style="none" />
-	</div>
-	<?php elseif ( $this->countModules('top-second-center') and 
-		($this->countModules('top-second-left') or $this->countModules('top-second-right')) ) : ?>
-	<div class="col-md-6">
-		<jdoc:include type="modules" name="top-second-center" style="none" />
-	</div>
-	<?php elseif ( $this->countModules('top-second-center') ) : ?>
-	<div class="col-md-12">
-		<jdoc:include type="modules" name="top-second-center" style="none" />
-	</div>
-	<?php endif; ?>
+		<?php // Top center column ?>
+		<?php if ( $this->countModules('top-second-center') and 
+			$this->countModules('top-second-left') and 
+			$this->countModules('top-second-right') ) : ?>
+		<div class="col-md-4">
+			<jdoc:include type="modules" name="top-second-center" style="none" />
+		</div>
+		<?php elseif ( $this->countModules('top-second-center') and 
+			($this->countModules('top-second-left') or $this->countModules('top-second-right')) ) : ?>
+		<div class="col-md-6">
+			<jdoc:include type="modules" name="top-second-center" style="none" />
+		</div>
+		<?php elseif ( $this->countModules('top-second-center') ) : ?>
+		<div class="col-md-12">
+			<jdoc:include type="modules" name="top-second-center" style="none" />
+		</div>
+		<?php endif; ?>
 
-	<?php // Top right column ?>
-	<?php if ( $this->countModules('top-second-right') and 
-		$this->countModules('top-second-left') and 
-		$this->countModules('top-second-center')) : ?>
-	<div class="col-md-4">
-		<jdoc:include type="modules" name="top-second-right" style="none" />
+		<?php // Top right column ?>
+		<?php if ( $this->countModules('top-second-right') and 
+			$this->countModules('top-second-left') and 
+			$this->countModules('top-second-center')) : ?>
+		<div class="col-md-4">
+			<jdoc:include type="modules" name="top-second-right" style="none" />
+		</div>
+		<?php elseif ( $this->countModules('top-second-right') and 
+			($this->countModules('top-second-left') or $this->countModules('top-second-center')) ) : ?>
+		<div class="col-md-6">
+			<jdoc:include type="modules" name="top-second-right" style="none" />
+		</div>
+		<?php elseif ( $this->countModules('top-second-right') ) : ?>
+		<div class="col-md-12">
+			<jdoc:include type="modules" name="top-second-right" style="none" />
+		</div>
+		<?php endif; ?>
 	</div>
-	<?php elseif ( $this->countModules('top-second-right') and 
-		($this->countModules('top-second-left') or $this->countModules('top-second-center')) ) : ?>
-	<div class="col-md-6">
-		<jdoc:include type="modules" name="top-second-right" style="none" />
-	</div>
-	<?php elseif ( $this->countModules('top-second-right') ) : ?>
-	<div class="col-md-12">
-		<jdoc:include type="modules" name="top-second-right" style="none" />
-	</div>
-	<?php endif; ?>
 </div>
 <?php echo $module_separator; ?>
 <?php endif; ?>
@@ -154,9 +158,11 @@
 
 <?php // Top Pre third. Модуль во всю ширину.==================================================== ?>
 <?php if ( $this->countModules('top-pre-third')) :?>
-<div class="row">
-	<div class="col-md-12">
-		<jdoc:include type="modules" name="top-pre-third" style="none" />
+<div class="row top-pre-third">
+	<div class="<?=$classContainer;?>">
+		<div class="col-md-12">
+			<jdoc:include type="modules" name="top-pre-third" style="none" />
+		</div>
 	</div>
 </div>
 <?php echo $module_separator; ?>
@@ -165,47 +171,49 @@
 
 <?php if ( $this->countModules('top-third-1') or $this->countModules('top-third-2') or 
 	$this->countModules('top-third-3') or $this->countModules('top-third-4')) : ?>
-<div class="row">
-	<?php // 4 very small===========================================================================?>
-	<?php // 1 ?>
-	<?php if ( $this->countModules('top-third-1') and 
-		$this->countModules('top-third-2') and 
-		$this->countModules('top-third-3') and 
-		$this->countModules('top-third-4')) : ?>
-	<div class="col-md-3">
-		<jdoc:include type="modules" name="top-third-1" style="none" />
-	</div>
-	<?php endif; ?>
+<div class="row top-third">
+	<div class="<?=$classContainer;?>">
+		<?php // 4 very small===========================================================================?>
+		<?php // 1 ?>
+		<?php if ( $this->countModules('top-third-1') and 
+			$this->countModules('top-third-2') and 
+			$this->countModules('top-third-3') and 
+			$this->countModules('top-third-4')) : ?>
+		<div class="col-md-3">
+			<jdoc:include type="modules" name="top-third-1" style="none" />
+		</div>
+		<?php endif; ?>
 
-	<?php // 2 ?>
-	<?php if ( $this->countModules('top-third-2') and 
-		$this->countModules('top-third-1') and 
-		$this->countModules('top-third-3') and 
-		$this->countModules('top-third-4')) : ?>
-	<div class="col-md-3">
-		<jdoc:include type="modules" name="top-third-2" style="none" />
-	</div>
-	<?php endif; ?>
+		<?php // 2 ?>
+		<?php if ( $this->countModules('top-third-2') and 
+			$this->countModules('top-third-1') and 
+			$this->countModules('top-third-3') and 
+			$this->countModules('top-third-4')) : ?>
+		<div class="col-md-3">
+			<jdoc:include type="modules" name="top-third-2" style="none" />
+		</div>
+		<?php endif; ?>
 
-	<?php // 3 ?>
-	<?php if ( $this->countModules('top-third-3') and 
-		$this->countModules('top-third-1') and 
-		$this->countModules('top-third-2') and 
-		$this->countModules('top-third-4')) : ?>
-	<div class="col-md-3">
-		<jdoc:include type="modules" name="top-third-3" style="none" />
-	</div>
-	<?php endif; ?>
+		<?php // 3 ?>
+		<?php if ( $this->countModules('top-third-3') and 
+			$this->countModules('top-third-1') and 
+			$this->countModules('top-third-2') and 
+			$this->countModules('top-third-4')) : ?>
+		<div class="col-md-3">
+			<jdoc:include type="modules" name="top-third-3" style="none" />
+		</div>
+		<?php endif; ?>
 
-	<?php // 4 ?>
-	<?php if ( $this->countModules('top-third-4') and 
-		$this->countModules('top-third-1') and 
-		$this->countModules('top-third-2') and 
-		$this->countModules('top-third-3')) : ?>
-	<div class="col-md-3">
-		<jdoc:include type="modules" name="top-third-4" style="none" />
+		<?php // 4 ?>
+		<?php if ( $this->countModules('top-third-4') and 
+			$this->countModules('top-third-1') and 
+			$this->countModules('top-third-2') and 
+			$this->countModules('top-third-3')) : ?>
+		<div class="col-md-3">
+			<jdoc:include type="modules" name="top-third-4" style="none" />
+		</div>
+		<?php endif; ?>
 	</div>
-	<?php endif; ?>
 </div>
 <?php echo $module_separator; ?>
 <?php endif; ?>
