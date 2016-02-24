cf8beeca107ab4f03aee1e14a835c8d8ee6615aa
whiskeyman <whiskeyman.tau@gmail.com>
2016-02-24 19:42:32 +0300

    develop commit

--- tmpls/footer.php
+++ tmpls/footer.php
@@ -1,5 +1,6 @@
   <footer class="footer" role="contentinfo">
     <div class="container<?php echo ($params->get('fluidContainer') ? '-fluid' : ''); ?>">
-  <h2><span>dsfdsaf</span></h2>
+      <h2><span>dsfdsaf</span></h2>
+      &copy; <?php echo date('Y'); ?> <?php echo $sitename; ?>
     </div>
   </footer>
