0900416edffb147d5ffa57694b2a01ccf9961172
whiskeyman <whiskeyman.tau@gmail.com>
2016-02-24 14:29:15 +0300

    develop commit

--- html/old_modules.php
+++ html/old_modules.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package     Joomla.Site
- * @subpackage  Templates.JBootstrap
+ * @subpackage  Templates.protostar
  *
  * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
@@ -54,29 +54,4 @@ function modChrome_well($module, &$params, &$attribs)
 			echo $module->content;
 		echo '</' . $moduleTag . '>';
 	}
-}
-
-/*
- * Bootstrap (divs and font header tags)
- * With the new advanced parameter it does the same as the html5 chrome and Twitter Bootstrap 4
- */
-function modChrome_bootstrap($module, &$params, &$attribs)
-{
-  $moduleTag      = $params->get('module_tag', 'div');
-  $headerTag      = htmlspecialchars($params->get('header_tag', 'h3'));
-  $bootstrapSize  = (int) $params->get('bootstrap_size', 0);
-  $moduleClass    = $bootstrapSize != 0 ? ' col-md-' . $bootstrapSize : '';
-
-  // Temporarily store header class in variable
-  $headerClass    = $params->get('header_class');
-  $headerClass    = ($headerClass) ? ' class="' . htmlspecialchars($headerClass) . '"' : '';
-
-  if (!empty ($module->content)) : ?>
-    <<?php echo $moduleTag; ?> class="moduletable<?php echo htmlspecialchars($params->get('moduleclass_sfx')) . $moduleClass; ?>">
-      <?php if ((bool) $module->showtitle) : ?>
-        <<?php echo $headerTag . $headerClass . '>' . $module->title; ?></<?php echo $headerTag; ?>>
-      <?php endif; ?>
-      <?php echo $module->content; ?>
-    </<?php echo $moduleTag; ?>>
-  <?php endif;
 }
